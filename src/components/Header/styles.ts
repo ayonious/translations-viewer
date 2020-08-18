99436d4b62a8a4daab89f4ecb0c90942deaa9e4d
Nahiyan Kamal <noyanayihan@gmail.com>
2020-08-19 00:31:47 +0200

    chore: fix tags

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 export const CenteredFlexDevWrapper = styled.div`
   display: flex;
   background-color: rgb(65, 80, 170);
-  @media only screen and (max-width: 1200px) {
-    max-width: 1200px;
-  }
   @media only screen and (min-width: 1200px) {
     width: 1200px;
+  }
+  @media only screen and (max-width: 1200px) {
+    width: 100vw;
   }
 `;
 
